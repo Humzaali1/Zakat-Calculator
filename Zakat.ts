00fc9c20dee25496ccc376f8816d9aa05f3a3ca3
Humza <hamzaali1141@gmail.com>
2024-04-03 00:51:31 +0500

    Ramzan zakat calculator

---
+++ Zakat.ts
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+
+let zakat: number = 2.5;
+let goldvalueinTola = 234800;
+let goldWeightInTolaReq = 7.5;
+
+let condition = true;
+while (condition){
+    const answer = await inquirer.prompt([
+        {
+            name: "goldweight",
+            message: "Enter gold weight:",
+            type: "number"
+    }
+    ]);
+    
+    let goldweight = answer.goldweight;
+    
+    if (goldweight >= goldWeightInTolaReq){
+        let calculatedzakat = (goldweight * goldvalueinTola) * (zakat / 100);
+        console.log(`The total calculatedvalue of gold weighing ${goldweight} tola is ${calculatedzakat}`)
+        // console.log ("The total calculatedvalue of gold weighing" goldweight + "tola is" + calculatedzakat); aese bhi likh skte hain
+     } else {
+            console.log("Zakat not applicable");
+            condition = false;
+        }
+    
+}
+
+// const answer = await inquirer.prompt([
+//     {
+//         name: "goldweight",
+//         message: "Enter gold weight:",
+//         type: "number"
+// }
+// ]);
+
+// let goldweight = answer.goldweight;
+
+// if (goldweight >= goldWeightInTolaReq){
+//     let calculatedzakat = (goldweight * goldvalueinTola) * (zakat / 100);
+//     console.log(`The total calculatedvalue of gold weighing ${goldweight} tola is ${calculatedzakat}`)
+//     // console.log ("The total calculatedvalue of gold weighing" goldweight + "tola is" + calculatedzakat); aese bhi likh skte hain
+//  } else {
+//         console.log("Zakat not applicable");
+//     }
+
+
+
